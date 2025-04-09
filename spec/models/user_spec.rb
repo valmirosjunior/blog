10f6e617998d5b1752d6aa506d8678efd5a7bd3c
Valmir Júnior <valmirosjunior@gmail.com>
2025-04-08 21:20:23 -0300

    feat: add tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:company) }
+  end
+
+  describe 'scopes' do
+    let!(:company_1) { create(:company) }
+    let!(:company_2) { create(:company) }
+    let!(:user_1) { create(:user, company: company_1, username: 'john_doe') }
+    let!(:user_2) { create(:user, company: company_1, username: 'jane_doe') }
+    let!(:user_3) { create(:user, company: company_2, username: 'john_smith') }
+
+    describe '.by_company' do
+      it 'returns users belonging to the specified company' do
+        expect(User.by_company(company_1.id)).to match_array([user_1, user_2])
+        expect(User.by_company(company_2.id)).to match_array([user_3])
+      end
+
+      it 'returns all users if no company_id is provided' do
+        expect(User.by_company(nil)).to match_array([user_1, user_2, user_3])
+      end
+    end
+
+    describe '.by_username' do
+      it 'returns users matching the username' do
+        expect(User.by_username('john')).to match_array([user_1, user_3])
+        expect(User.by_username('jane')).to match_array([user_2])
+      end
+
+      it 'returns all users if no username is provided' do
+        expect(User.by_username(nil)).to match_array([user_1, user_2, user_3])
+      end
+    end
+  end
 end
