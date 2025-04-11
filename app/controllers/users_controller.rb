ecb115a8bc1125f48c7c3146b96e41d6bf5b15f4
Valmir Júnior <valmirosjunior@gmail.com>
2025-04-10 23:39:33 -0300

    feat: add create user on companies show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,26 @@
-class UsersController < ApiController
+class UsersController < ApplicationController
+  before_action :set_company, only: [:new, :create]
 
   def index
     users = User
               .by_company(params[:company_id])
               .by_username(search_params[:username])
+
     render json: users.all
+  end
+
+  def new
+    @user = @company.users.build
+  end
+
+  def create
+    @user = @company.users.build(user_params)
+
+    if @user.save
+      redirect_to company_path(@company), notice: 'User was successfully created.'
+    else
+      render :new
+    end
   end
 
   private
@@ -13,4 +29,11 @@ class UsersController < ApiController
     params.permit(:username)
   end
 
+  def set_company
+    @company = Company.find(params[:company_id])
+  end
+
+  def user_params
+    params.require(:user).permit(:display_name, :email, :username)
+  end
 end
