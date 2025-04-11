9d7d2327012410f6087d3246c1a4c16c74a50463
Valmir Júnior <valmirosjunior@gmail.com>
2025-04-11 09:58:43 -0300

    fix: add missing tests

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,5 @@
 class Company < ApplicationRecord
   has_many :users, dependent: :restrict_with_error
+
+  validates :name, presence: true
 end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should have_many(:users).dependent(:restrict_with_error) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+  end
+
+  describe 'dependent destroy behavior' do
+    let!(:company) { create(:company) }
+    let!(:user) { create(:user, company: company) }
+
+    context 'when the company has associated users' do
+      it 'does not allow the company to be destroyed' do
+        expect { company.destroy }.not_to change(Company, :count)
+
+        expect(company.errors[:base]).to include("Cannot delete record because dependent users exist")
+      end
+    end
+
+    context 'when the company has no associated users' do
+      before { user.destroy }
+
+      it 'allows the company to be destroyed' do
+        expect { company.destroy }.to change(Company, :count).by(-1)
+      end
+    end
+  end
 end
